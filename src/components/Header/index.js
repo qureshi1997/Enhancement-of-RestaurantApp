345fc0993dab56f4bcc574900d0e11264810e89a
qureshi1997 <qureshibrothers1997@gmail.com>
2024-07-23 19:59:40 +0000

    solution

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,30 +17,30 @@ const Header = props => {
   }
 
   const renderCartIcon = () => (
-    <div className="cart-icon-link">
-      <Link to="/cart">
-        <button type="button" className="cart-icon-button" data-testid="cart">
-          <AiOutlineShoppingCart className="cart-icon" />
+    <div className='cart-icon-link'>
+      <Link to='/cart'>
+        <button type='button' className='cart-icon-button' data-testid='cart'>
+          <AiOutlineShoppingCart className='cart-icon' />
         </button>
       </Link>
-      <div className="cart-count-badge d-flex justify-content-center align-items-center">
-        <p className="m-0 cart-count">{cartList.length}</p>
+      <div className='cart-count-badge d-flex justify-content-center align-items-center'>
+        <p className='m-0 cart-count'>{cartList.length}</p>
       </div>
     </div>
   )
 
   return (
-    <header className="p-4 d-flex flex-row align-items-center nav-header">
-      <Link to="/">
-        <h1 className="m-0 logo-heading">{restaurantName}</h1>
+    <header className='p-4 d-flex flex-row align-items-center nav-header'>
+      <Link to='/'>
+        <h1 className='m-0 logo-heading'>{restaurantName}</h1>
       </Link>
-      <div className="d-flex flex-row align-items-center ms-auto">
-        <p className="mt-0 mb-0 me-2 d-none d-sm-block my-orders-text">
+      <div className='d-flex flex-row align-items-center ms-auto'>
+        <p className='mt-0 mb-0 me-2 d-none d-sm-block my-orders-text'>
           My Orders
         </p>
         <button
-          type="button"
-          className="btn btn-outline-danger ms-2 me-2 btn-sm"
+          type='button'
+          className='btn btn-outline-danger ms-2 me-2 btn-sm'
           onClick={onLogout}
         >
           Logout
